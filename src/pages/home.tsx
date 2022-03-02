818b4bf092ded778c6b93e0fd80beb88226aabd7
rustin <1543153730@qq.com>
2022-03-02 21:00:40 +0800

    添加header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/header.png';
 
 export const Home = () => {
-  return (
-    <div>
-      你好
-    </div>
-  );
+    return (
+        <div className='home'>
+            <header style={{ backgroundImage: `url(${headerBg})` }}></header>
+        </div>
+    );
 };
