34cb632c3e383d9435ad60804a6c36b3e7c4a665
rustin <1543153730@qq.com>
2022-03-03 20:13:14 +0800

    chart1

---
+++ src/components/chart-2.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
+
+const px = (n) => (n / 2420) * (window as any).pWidth;
+export const Chart2 = () => {
+    const divRef = useRef(null);
+    useEffect(() => {
+        var myChart = echarts.init(divRef.current);
+        myChart.setOption({
+            textStyle: {
+                fontSize: px(12),
+                color: '#79839E',
+            },
+            title: { show: false },
+            legend: { show: false },
+            xAxis: {
+                data: [
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                    '兰州新区',
+                ],
+                axisTick: { show: false },
+                axisLine: {
+                    lineStyle: { color: '#083B70' },
+                },
+                axisLabel: {
+                    fontSize: px(12),
+                    formatter(val) {
+                        if (val.length > 2) {
+                            const array = val.split('');
+                            array.splice(2, 0, '\n');
+                            return array.join('');
+                        } else {
+                            return val;
+                        }
+                    },
+                },
+            },
+            grid: {
+                x: px(40),
+                y: px(40),
+                x2: px(40),
+                y2: px(40),
+            },
+            yAxis: {
+                splitLine: { show: false },
+                axisLine: {
+                    show: true,
+                    lineStyle: { color: '#083B70' },
+                },
+                axisLabel: {
+                    fontSize: px(12),
+                },
+            },
+            series: [
+                {
+                    type: 'bar',
+                    data: [10, 20, 36, 41, 15, 26, 37, 18, 29],
+                },
+            ],
+        });
+    }, []);
+
+    return (
+        <div className='bordered guanxiatongji'>
+            <h2>案发派出所管辖统计</h2>
+            <div ref={divRef} className='chart'></div>
+        </div>
+    );
+};
